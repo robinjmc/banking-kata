1bfb6a261aad1317204655ce2d4a43436d40985a
Robin Cunningham <robin.cunningham@thinkmoney.co.uk>
2021-06-24 11:51:52 +0100

    first swarm

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-function sum(a, b) {
-    return a + b;
+class Account {
+
+    constructor() {
+        this.bankBalance = 0;
+        this.transactionsLog = [];
+    }
+
+    deposit(amount) {
+        this.bankBalance += amount;
+    }
+
+    withdraw(amount) {
+        this.bankBalance -= amount;
+    }
+
+    logTransaction(date, amount) {
+        this.transactionsLog.push({ date: date, amount: amount })
+    }
 }
+
 module.exports = {
-    sum,
+    Account,
 };
--- index.test.js
+++ index.test.js
@@ -1,5 +1,43 @@
-const { sum } = require('./index.js');
+const { Account } = require('./index.js');
 
-test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
+test('can create a new account with a bank balance', () => {
+    let account = new Account();
+    let expectedObject = { bankBalance: 0, transactionsLog: [] };
+    expect(account).toMatchObject(expectedObject);
 });
+
+test('it can deposit funds', () => {
+    let account = new Account();
+    account.deposit(100);
+    expect(account.bankBalance).toBe(100);
+});
+
+test('it can deposit funds multiple times', () => {
+    let account = new Account();
+    account.deposit(100);
+    expect(account.bankBalance).toBe(100);
+    account.deposit(150);
+    expect(account.bankBalance).toBe(250);
+});
+
+test('it can withdraw funds', () => {
+    let account = new Account();
+    account.deposit(1000);
+    expect(account.bankBalance).toBe(1000);
+    account.withdraw(500);
+    expect(account.bankBalance).toBe(500);
+});
+
+test('record transactions', () => {
+    let account = new Account();
+    const date = '10/01/2021';
+    const amount = 300;
+    account.logTransaction(date, amount);
+    const expectedObject = { date: date, amount: amount };
+    expect(account.transactionsLog[0]).toMatchObject(expectedObject);
+});
+
+// dates
+
+
+// print transactions
